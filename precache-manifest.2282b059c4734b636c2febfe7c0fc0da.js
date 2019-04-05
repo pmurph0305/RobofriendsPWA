d5eb2025916f5c694b6967e3b584e0a51dd947a2
pmurph0305 <pmurph0305@gmail.com>
2019-04-05 14:06:13 -0400

    Updates

---
+++ precache-manifest.2282b059c4734b636c2febfe7c0fc0da.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/RobofriendsPWA/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "980db1e6293f92637bff",
+    "url": "/RobofriendsPWA/static/js/runtime~main.35934667.js"
+  },
+  {
+    "revision": "9b2d35f8560735b64a58",
+    "url": "/RobofriendsPWA/static/js/main.134f976e.chunk.js"
+  },
+  {
+    "revision": "485529a148d04fd04b7d",
+    "url": "/RobofriendsPWA/static/js/2.ab4a082b.chunk.js"
+  },
+  {
+    "revision": "9b2d35f8560735b64a58",
+    "url": "/RobofriendsPWA/static/css/main.5ce12b1c.chunk.css"
+  },
+  {
+    "revision": "485529a148d04fd04b7d",
+    "url": "/RobofriendsPWA/static/css/2.06245324.chunk.css"
+  },
+  {
+    "revision": "4af9e1d239bd6c5050d1c9b84c24a358",
+    "url": "/RobofriendsPWA/index.html"
+  }
+];
